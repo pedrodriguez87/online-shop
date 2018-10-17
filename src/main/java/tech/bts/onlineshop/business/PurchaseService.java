76bef13cde817a6e1ee054deda9995ca9e1e2330
Absolutt87 <pedrodriguez87@hotmail.com>
2018-10-17 11:43:39 +0200

    Create PurchaseService (not finished)

---
+++ src/main/java/tech/bts/onlineshop/business/PurchaseService.java
@@ -0,0 +1,18 @@
+package tech.bts.onlineshop.business;
+
+import tech.bts.onlineshop.model.Product;
+import tech.bts.onlineshop.model.ShoppingCart;
+
+import java.util.List;
+
+public class PurchaseService {
+
+    /**
+     * Returns the total price of the cart
+     *
+     */
+    public double calculateTotalAmount(ShoppingCart cart) {
+
+        return 0;
+    }
+}
